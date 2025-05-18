325a0645a61d3ecd885056090c56dec272a887b1
Omprakash-Jangid <omprakash_j@mfs.iitr.ac.in>
2025-05-18 19:40:59 +0530

    fix

--- obuga_project/settings.py
+++ obuga_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(Path(__file__).resolve().parent.parent, 'attendance', 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(Path(__file__).resolve().parent.parent, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
