fed2b80c7a2bfa5ff8a8391a91720f4337417087
Omprakash-Jangid <omprakash_j@mfs.iitr.ac.in>
2025-05-18 17:00:10 +0530

    superadmin fix

--- attendance/management/commands/create_superadmin.py
+++ attendance/management/commands/create_superadmin.py
@@ -1,19 +1,18 @@
-# create_superuser.py
+from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
-import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourproject.settings")
-django.setup()
+class Command(BaseCommand):
+    help = 'Creates superadmin user if it does not exist'
 
-User = get_user_model()
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpass')
 
-username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpass')
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print("Superuser created")
-else:
-    print("Superuser already exists")
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(f'Superuser {username} already exists.')
+        else:
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(f'Superuser {username} created.')
